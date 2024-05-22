c48247c5fa16bc7a7f5fa34b1744e5ada4502874
Daniel Attali <danielattali16@gmail.com>
2024-05-22 13:12:44 +0300

    continue the work on the game part

--- fswd-4/src/components/gameCell/gameCell.jsx
+++ fswd-4/src/components/gameCell/gameCell.jsx
@@ -2,9 +2,25 @@ import { useState } from "react";
 
 import PropTypes from "prop-types";
 
+function handleGamePlay(setNumber, setStep, setIsDone, number, step, operation) {
+  if (number == 100) {
+    setIsDone(true);
+  }
+
+  let n = operation(number);
+
+  setNumber(n);
+  setStep(step + 1);
+
+  if (n == 100) {
+    setIsDone(true);
+  }
+}
+
 export default function GameCell({ name, state, random }) {
   const [number, setNumber] = useState(random);
   const [step, setStep] = useState(1);
+  const [isDone, setIsDone] = useState(false);
 
   return (
     <div className="game-cell">
@@ -16,33 +32,56 @@ export default function GameCell({ name, state, random }) {
       <div className="controller">
         <button
           onClick={() => {
-            setNumber(number + 1);
-            setStep(step + 1);
+            handleGamePlay(
+              setNumber,
+              setStep,
+              setIsDone,
+              number,
+              step,
+              (n) => n + 1
+            );
           }}
+          disabled={isDone || !state}
         >
           +1
         </button>
         <button
           onClick={() => {
-            setNumber(number - 1);
-            setStep(step + 1);
+            handleGamePlay(
+              setNumber,
+              setStep,
+              setIsDone,
+              number,
+              step,
+              (n) => n - 1
+            );
           }}
+          disabled={isDone || !state}
         >
           -1
         </button>
         <button
           onClick={() => {
-            setNumber(number * 2);
-            setStep(step + 1);
+            handleGamePlay(
+              setNumber,
+              setStep,
+              setIsDone,
+              number,
+              step,
+              (n) => n * 2
+            );
           }}
+          disabled={isDone || !state}
         >
           * 2
         </button>
         <button
           onClick={() => {
-            setNumber(Math.round(number / 2));
-            setStep(step + 1);
+            handleGamePlay(setNumber, setStep, setIsDone, number, step, (n) =>
+              Math.round(n / 2)
+            );
           }}
+          disabled={isDone || !state}
         >
           / 2
         </button>
--- fswd-4/src/pages/game/game.jsx
+++ fswd-4/src/pages/game/game.jsx
@@ -1,16 +1,86 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import GameCell from "../../components/gameCell/gameCell"
+import GameCell from "../../components/gameCell/gameCell";
+
+const gamers = [{ name: "Gamer 1", state: false, games: [8, 40, 12] }];
+
+/*
+The gamer will be stored in the localStorage
+write a function to save the gamer in the localStorage
+write a function to get the gamer from the localStorage
+
+gamer = {
+  name: string,
+  state: boolean, : playing not playing
+  games: [number] : history of games
+}
+*/
+
+function saveGamers(gamers) {
+  localStorage.setItem("gamers", JSON.stringify(gamer));
+}
+
+function getGamers() {
+  const gamers = localStorage.getItem("gamers");
+  return JSON.parse(gamers);
+}
 
 export default function Game() {
-    const [gamer, ] = useState([{name: "Daniel Attali", state: true, random: 43}])
+  const [gamer, setGamer] = useState(gamers);
 
-    return <>
-        <h1>Game</h1>
-        <p>Game content</p>
+  return (
+    <>
+      <h1>Game</h1>
+      <p>Game content</p>
 
-        <div className="game-cell">
-            {gamer.map((g, i) => <GameCell key={i} name={g.name} state={g.state} random={g.random} />)}
+      <div className="controller">
+        <div className="new-gamer">
+          <input type="text" placeholder="Gamer Name" />
+          <button>Add New Gamer</button>
         </div>
+        <div className="start-game">
+          <button
+            onClick={() => {
+              // change the state of the gamer to true
+
+              let newGamer = gamer.map((g) => {
+                return {
+                  ...g,
+                  state: true,
+                };
+              });
+
+              setGamer(newGamer);
+            }}
+          >
+            Start Game
+          </button>
+        </div>
+      </div>
+
+      <div className="game-cell">
+        {gamer.map((g, i) => (
+          <GameCell
+            key={i}
+            name={g.name}
+            state={g.state}
+            random={Math.floor(Math.random() * 100)}
+          />
+        ))}
+        <div className="game-history">
+          <ul>
+            {gamer.map((g, i) => (
+              <li key={i}>
+                <ul>
+                  {g.games.map((game, j) => (
+                    <li key={j}>{game}</li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </>
+  );
 }
