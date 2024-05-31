dda4b9a9d439356acce81b5313a5f86703b68aea
lior serraf <lior.serraf@gmail.com>
2024-05-31 04:33:38 +0300

    turn based all changements

--- fswd-4/src/components/gameCell/gameCell.jsx
+++ fswd-4/src/components/gameCell/gameCell.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 
 import PropTypes from "prop-types";
 
@@ -9,51 +10,89 @@ function handleGamePlay(
   number,
   step,
   operation,
-  handleGameEnd
+  handleGameEnd,
+  handlePlayerTurnEnd,
+  gamerName
 ) {
-  if (number === 100) {
-    setIsDone(true);
-    handleGameEnd();
-  }
+  // find the gamer in the local storage for update
+  const storedGamers = JSON.parse(localStorage.getItem('gamers')) || [];
+  const gamerIndex = storedGamers.findIndex(g => g.name === gamerName);
+  const gamer = storedGamers[gamerIndex];
 
-  let n = operation(number);
+  if (!gamer) return;
 
+  let n = operation(gamer.score);
   setNumber(n);
   setStep(step + 1);
 
+  gamer.score = n; 
+ 
   if (n === 100) {
-    setIsDone(true);
-    handleGameEnd();
+    handleGameEnd(gamer);
+  } else {
+    handlePlayerTurnEnd(gamer.name);
   }
+  storedGamers[gamerIndex] = gamer;
+  localStorage.setItem('gamers', JSON.stringify(storedGamers));
 }
 
 export default function GameCell({
   gamer,
+  isGameActive,
   handleQuitPlayer,
-  updateLeaderboard,
+  handleEnd,
+  handleJoinLeave,
+  handlePlayerTurnEnd,
+  isPlaying
 }) {
   const [number, setNumber] = useState(0);
   const [step, setStep] = useState(1);
   const [isDone, setIsDone] = useState(false);
-  const [isPlaying, setIsPlaying] = useState(false);
+  const [isJoined, setIsJoined] = useState(false);
+  const [isTurn, setIsTurn] = useState(gamer.isTurn);
 
-  const handleStartGame = () => {
-    setIsPlaying(true);
-    setNumber(Math.floor(Math.random() * 100));
-    setStep(1);
-    setIsDone(false);
+//updates with the buttons
+  useEffect(() => {
+    setIsTurn(gamer.isTurn);
+  }, [gamer.isTurn]);
+
+  useEffect(() => {
+    setNumber(gamer.score);
+  }, [gamer.score]);
+
+  useEffect(() => {
+    setIsJoined(gamer.joined);
+  }, [gamer.joined]);
+
+  useEffect(() => {
+    if (isGameActive) {
+      setIsTurn(gamer.isTurn)
+      setStep(0);
+    }
+}, [isGameActive]);
+
+
+  const handleJoin = () => {
+    setIsJoined(true);
+    handleJoinLeave(gamer.name, true);
+  };
+
+  const handleLeave = () => {
+    setIsJoined(false);
+    handleJoinLeave(gamer.name, false);
   };
 
   const handleGameEnd = () => {
-    setIsPlaying(false);
-    gamer.games.push(step);
-    const storedGamers = JSON.parse(localStorage.getItem("gamers")) || [];
+    window.alert(`${gamer.name} has won`);
+    setIsTurn(false);
+    gamer.games.push(step); //update for history
+    const storedGamers = JSON.parse(localStorage.getItem('gamers')) || [];
     const updatedGamers = storedGamers.map((storedGamer) =>
       storedGamer.name === gamer.name ? gamer : storedGamer
     );
-    localStorage.setItem("gamers", JSON.stringify(updatedGamers));
+    localStorage.setItem('gamers', JSON.stringify(updatedGamers));
 
-    updateLeaderboard(updatedGamers);
+    handleEnd(updatedGamers);
   };
 
   return (
@@ -64,98 +103,47 @@ export default function GameCell({
       <p>Status: {isPlaying ? "Active" : "Inactive"}</p>
 
       <div className="action-buttons">
-        {isPlaying ? (
+        {isPlaying && isJoined && isTurn ? (
           <>
-            <button
-              className="action-button"
-              onClick={handleGamePlay(
-                setNumber,
-                setStep,
-                setIsDone,
-                number,
-                step,
-                (n) => n + 1,
-                handleGameEnd
-              )}
-              disabled={isDone}
-            >
-              +1
-            </button>
-            <button
-              className="action-button"
-              onClick={handleGamePlay(
-                setNumber,
-                setStep,
-                setIsDone,
-                number,
-                step,
-                (n) => n - 1,
-                handleGameEnd
-              )}
-              disabled={isDone}
-            >
-              -1
-            </button>
-            <button
-              className="action-button"
-              onClick={handleGamePlay(
-                setNumber,
-                setStep,
-                setIsDone,
-                number,
-                step,
-                (n) => n * 2,
-                handleGameEnd
-              )}
-              disabled={isDone}
-            >
-              *2
-            </button>
-            <button
-              className="action-button"
-              onClick={handleGamePlay(
-                setNumber,
-                setStep,
-                setIsDone,
-                number,
-                step,
-                (n) => n / 2,
-                handleGameEnd
-              )}
-              disabled={isDone}
-            >
-              /2
-            </button>
+            <button className="action-button" onClick={() => handleGamePlay(setNumber, setStep, setIsDone, number, step, n => n + 1, handleGameEnd, handlePlayerTurnEnd, gamer.name)} disabled={isDone}>+1</button>
+            <button className="action-button" onClick={() => handleGamePlay(setNumber, setStep, setIsDone, number, step, n => n - 1, handleGameEnd, handlePlayerTurnEnd, gamer.name)} disabled={isDone}>-1</button>
+            <button className="action-button" onClick={() => handleGamePlay(setNumber, setStep, setIsDone, number, step, n => n * 2, handleGameEnd, handlePlayerTurnEnd, gamer.name)} disabled={isDone}>*2</button>
+            <button className="action-button" onClick={() => handleGamePlay(setNumber, setStep, setIsDone, number, step, n => Math.floor(n / 2), handleGameEnd, handlePlayerTurnEnd, gamer.name)} disabled={isDone}>/2</button>
+          </>
+        ) : isGameActive && (!isTurn || !isJoined) ? (
+          <>
+            <button className="disabled-button" disabled>Waiting...</button>
           </>
         ) : (
           <>
-            <button className="green-button" onClick={handleStartGame}>
-              Start Game
-            </button>
-            <button
-              className="red-button"
-              onClick={() => handleQuitPlayer(gamer.name)}
-            >
-              Quit Game
-            </button>
+            {isJoined ? (
+              <>
+                <button className="orange-button" onClick={() => handleLeave()}>Leave Game</button>
+                <button className="red-button" onClick={() => handleQuitPlayer(gamer.name)}>Quit Game</button>
+              </>
+            ) : (
+              <>
+                <button className="green-button" onClick={() => handleJoin(gamer.name, true)}>Join Game</button>
+                <button className="red-button" onClick={() => handleQuitPlayer(gamer.name)}>Quit Game</button>
+              </>
+            )}
           </>
         )}
       </div>
       <div className="game-history">
         <p>Previous games: {gamer.games.join(", ")}</p>
       </div>
-      {/* <button
-        className="red-button"
-        onClick={() => handleQuitPlayer(gamer.name)}
-      >
-        Quit Game
-      </button> */}
     </div>
   );
 }
 
 GameCell.propTypes = {
   gamer: PropTypes.object.isRequired,
+  isGameActive: PropTypes.bool.isRequired,
   handleQuitPlayer: PropTypes.func.isRequired,
-  updateLeaderboard: PropTypes.func.isRequired,
+  handleEnd: PropTypes.func.isRequired,
+  handleJoinLeave: PropTypes.func.isRequired,
+  handlePlayerTurnEnd: PropTypes.func.isRequired,
+  isPlaying: PropTypes.bool.isRequired,
 };
+
--- fswd-4/src/pages/game/game.jsx
+++ fswd-4/src/pages/game/game.jsx
@@ -1,87 +1,85 @@
-/* eslint-disable react/prop-types */
 import "./game.css";
 
-import { useEffect, useState } from "react";
+import { useState} from "react";
 
 import GameCell from "../../components/gameCell/gameCell";
 
-import { getGamers } from "../../utils/gamers";
+function getGamers() {
+  const storedGamers = localStorage.getItem('gamers');
+  if (storedGamers) {
+    return JSON.parse(storedGamers);
+  }
+  return [];
+}
+
 
 export default function Game() {
-  const [gamer, setGamer] = useState([...getGamers()]);
+  const [gamers, setGamers] = useState([...getGamers()]);
   const [leaderboard, setLeaderboard] = useState([]);
+  const [isGameActive, setIsGameActive] = useState(false);
 
-  const updateLeaderboard = updateLeaderboardF(setLeaderboard);
-  const handleAddPlayer = handleAddPlayerF(setGamer);
-  const handleQuitPlayer = handleQuitPlayerF(setGamer);
 
-  useEffect(() => {
-    updateLeaderboard(gamer);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [gamer]);
+  const handleAddPlayer = (name) => {
+    let currentGamers  = getGamers();
+    let gamerIndex = currentGamers.findIndex((g) => g.name === name);
 
-  const handleInput = () => {
-    let input = document.getElementById("gamer-name");
-    if (input.value.trim() === "") {
-      alert("Name is required to create a new player.");
-      return;
+    if (gamerIndex === -1) {
+      let newGamer = {
+        name: name,
+        state: false,
+        games: [],
+        playing: true,
+        joined:false,
+        isTurn:false,
+      };
+      currentGamers .push(newGamer);
+      localStorage.setItem('gamers', JSON.stringify(currentGamers ));
+      setGamers([...currentGamers]);
+    } else {
+      currentGamers [gamerIndex].playing = true;
+      localStorage.setItem('gamers', JSON.stringify(currentGamers ));
+      setGamers([...currentGamers]);
     }
-    handleAddPlayer(input.value);
   };
 
-  return (
-    <>
-      <div className="header">
-        <h1 className="center-title">Get to 100!</h1>
-        <div className="controller">
-          <div className="new-gamer">
-            <input type="text" placeholder="Gamer Name" id="gamer-name" />
-            <button className="orange-button" onClick={handleInput}>
-              Add New Gamer
-            </button>
-          </div>
-        </div>
-      </div>
+  const handleStartGame = () => {
+    const activePlayers = gamers.filter(g => g.joined && g.playing);
+  if (activePlayers.length < 2) {
+    window.alert("At least 2 players are required to start the game.");
+    return;
+  }
+    setIsGameActive(true);
+    if (activePlayers.length > 0) {
+        setGamers(currentGamers => {
+            const updatedGamers = currentGamers.map(gamer => ({
+                ...gamer,
+                isTurn: gamer.name === activePlayers[0].name,
+                score: Math.floor(Math.random() * 99)+1 , 
+                
+            }));
 
-      <div className="game-container">
-        <div className="game-cell-container">
-          {gamer
-            .filter((g) => g.playing)
-            .map((g, i) => (
-              <GameCell
-                key={i}
-                gamer={g}
-                handleQuitPlayer={handleQuitPlayer}
-                updateLeaderboard={updateLeaderboard}
-                gamers={gamer}
-              />
-            ))}
-        </div>
+            localStorage.setItem('gamers', JSON.stringify(updatedGamers));
 
-        {LeaderBoard(leaderboard)}
-      </div>
-    </>
-  );
-}
+            return updatedGamers;
+        });
+    }
+};
+  const handlePlayerTurnEnd = (name) => {
+    const activePlayers = gamers.filter(g => g.joined && g.playing);
+    let nextPlayerIndex = activePlayers.findIndex(g => g.name === name) + 1;
+    if (nextPlayerIndex >= activePlayers.length) {
+      nextPlayerIndex = 0;
+    }
+    setGamers((currentGamers) => currentGamers.map(gamer => ({
+      ...gamer,
+      isTurn: gamer.name === activePlayers[nextPlayerIndex].name
+    })));
+  };
+  
+  
+  const handleEnd = (gamers) => {
+    setIsGameActive(false);
 
-function LeaderBoard(leaderboard) {
-  return (
-    <div className="leaderboard">
-      <h2>Leaderboard</h2>
-      <ol>
-        {leaderboard.map((player, index) => (
-          <li key={index} className={`leaderboard-item rank-${index + 1}`}>
-            {player.name}:{" "}
-            {player.bestScore === Infinity ? "N/A" : player.bestScore} turns
-          </li>
-        ))}
-      </ol>
-    </div>
-  );
-}
-
-function updateLeaderboardF(setLeaderboard) {
-  return (gamers) => {
     const updatedLeaderboard = gamers
       .map((g) => ({
         name: g.name,
@@ -91,39 +89,88 @@ function updateLeaderboardF(setLeaderboard) {
       .slice(0, 3);
     setLeaderboard(updatedLeaderboard);
   };
-}
 
-function handleQuitPlayerF(setGamer) {
-  return (name) => {
-    setGamer((prevGamers) => {
+  const handleQuitPlayer = (name) => {
+    setGamers((prevGamers) => {
       const updatedGamers = prevGamers.map((g) =>
         g.name === name ? { ...g, playing: false } : g
       );
-      localStorage.setItem("gamers", JSON.stringify(updatedGamers));
+      localStorage.setItem('gamers', JSON.stringify(updatedGamers));
       return updatedGamers;
     });
   };
-}
 
-function handleAddPlayerF(setGamer) {
-  return (name) => {
-    let gamers = getGamers();
-    let gamerIndex = gamers.findIndex((g) => g.name === name);
-
-    if (gamerIndex === -1) {
-      let newGamer = {
-        name: name,
-        state: false,
-        games: [],
-        playing: true,
-      };
-      gamers.push(newGamer);
-      localStorage.setItem("gamers", JSON.stringify(gamers));
-      setGamer([...gamers]);
-    } else {
-      gamers[gamerIndex].playing = true;
-      localStorage.setItem("gamers", JSON.stringify(gamers));
-      setGamer([...gamers]);
-    }
+  const handleJoinLeave = (name, joinStatus) => {
+    setGamers((prevGamers) => {
+      const updatedGamers = prevGamers.map((g) => 
+        g.name === name ? { ...g, joined: joinStatus } : g
+      );
+      localStorage.setItem('gamers', JSON.stringify(updatedGamers));
+      return updatedGamers;
+    });
   };
+  
+
+  return (
+    <>
+      <div className="header">
+        <h1 className="center-title">Get to 100!</h1>
+        <div className="controller">
+          <div className="new-gamer">
+            <input type="text" placeholder="Gamer Name" id="gamer-name" />
+            <button
+              className="orange-button"
+              onClick={() => {
+                let input = document.getElementById("gamer-name");
+                if (input.value.trim() === "") {
+                  alert("Name is required to create a new player.");
+                  return;
+                }
+                handleAddPlayer(input.value);
+              }}
+            >
+              Add New Gamer
+            </button>
+          </div>
+          { !isGameActive && (
+              <button
+                className="green-button"
+                onClick={handleStartGame}
+              >
+                Start Game
+              </button>
+            )}
+
+        </div>
+      </div>
+
+      <div className="game-container">
+        <div className="game-cell-container">
+          {gamers.filter((g) => g.playing).map((g, i) => (
+            <GameCell
+              key={i}
+              gamer={g}
+              isGameActive={isGameActive}
+              handleQuitPlayer={handleQuitPlayer}
+              handleEnd={handleEnd}
+              handleJoinLeave={handleJoinLeave}
+              handlePlayerTurnEnd={handlePlayerTurnEnd}
+              isPlaying={g.playing} 
+            />
+          ))}
+        </div>
+
+        <div className="leaderboard">
+          <h2>Leaderboard</h2>
+          <ol>
+            {leaderboard.map((player, index) => (
+              <li key={index} className={`leaderboard-item rank-${index + 1}`}>
+                {player.name}: {player.bestScore === Infinity ? "N/A" : player.bestScore} turns
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    </>
+  );
 }
